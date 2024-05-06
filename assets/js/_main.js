e0a97cb8ee5a09f9fecdeb6149a750b3eade4db3
kewtgh <horolfmax@gmail.com>
2024-05-06 17:18:04 +0800

    3.0.7

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -81,7 +81,7 @@ $(function () {
   // add lightbox class to all image links
   $(
     "a[href$='.jpg'],a[href$='.jpeg'],a[href$='.JPG'],a[href$='.png'],a[href$='.gif'],a[href$='.webp']"
-  ).addClass("image-popup");
+  ).has("> img").addClass("image-popup");
 
   // Magnific-Popup options
   $(".image-popup").magnificPopup({
